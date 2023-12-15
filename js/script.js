cc687c4e0ba1fe634517bd7fc3848f4755de35db
MateuszKacperski <kacperskimateusz95@gmail.com>
2023-12-15 14:47:06 +0100

    added variables on js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 console.log('JS OK');
+
+
+//Dichiaro le variabili 
+/* le variabili sono tutte let in quanto verranno sovrascritte. 
+L'unica const e la stringa con il numero in quanto sarà sempre uguale*/
+let firstname= '';
+let surname='';
+let color='';
+let password='';
+const number='21';
+
+console.log(firstname, surname, color, password, number);
+
